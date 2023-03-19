d8ce92566014c37dd22b4b2c58709ba5a277a6f4
wangyu <2514825513@qq.com>
2023-03-19 17:05:54 +0800

    MOD: change interface to https

--- craco.config.js
+++ craco.config.js
@@ -31,7 +31,7 @@ module.exports = {
       // 将来以/dev-api开头的请求，就会被开发服务器转发到目标服务器去。
       "/dev-api": {
         // 需要转发的请求前缀
-        target: 'http://www.wangyu.cloud:7000',
+        target: 'https://www.wangyu.cloud:7000',
         changeOrigin: true, // 为true时代理在转发时, 会将请求头的host改为target的值
         pathRewrite: { // 路径重写
           "^/dev-api": "", // 在转发请求时去掉多的/dev-api部分
